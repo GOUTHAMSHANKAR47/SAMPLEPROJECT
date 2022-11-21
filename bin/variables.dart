b6a517c6beee05b951ac0f0cbf4647ad8669b892
GOUTHAMSHANKR47 <gsgouthamgs2@gmail.com>
2022-11-21 15:14:36 +0530

    commit

---
+++ bin/variables.dart
@@ -0,0 +1,14 @@
+void main(){
+  String name = "abu";
+  int age = 18;
+  int phone = 8157980981;
+  double mark = 9.8;
+  String email = "abusworld12@gmail.com";
+
+  print("++++ABU'S RESUME++++");
+  print("my name is ${name}");  //string interpolation =${variablename}
+  print("my age is ${age}");
+  print("my phone number is ${phone}");
+  print("my cgpa is ${mark}");
+  print("my email is ${email}");
+}
