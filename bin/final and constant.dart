61a90ce0b4410c38d45669f9a5b019ebf907d0dd
GOUTHAMSHANKR47 <gsgouthamgs2@gmail.com>
2022-11-22 16:13:43 +0530

    commit

---
+++ bin/final and constant.dart
@@ -0,0 +1,21 @@
+void main(){
+  String name = "anna";
+  int age = 99;
+  final school = "chss";
+  final pincode = 670694;
+
+
+  print(name);
+  print(age);
+  print(school);
+  print(pincode);
+
+  name = "komu";
+  age = 19;
+
+  print(name);
+  print(age);
+  print(school);
+  print(pincode);
+
+}
